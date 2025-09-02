7a3daf5b6aa1a0f2d342cacb84708006896bd513
Umar0260 <mohdumar0260@gmail.com>
2025-09-02 19:47:47 +0530

    variable chapter comlete

---
+++ variables.js
@@ -0,0 +1,8 @@
+//In javascript there are three tpes of variables
+
+const PI= 3.14;  // containt constant value only.
+var studenName = "Umar" // functional scope
+let studentCity = "Delhi" // block scope
+age = 30
+
+console.table([PI, studenName, studentCity, age])
